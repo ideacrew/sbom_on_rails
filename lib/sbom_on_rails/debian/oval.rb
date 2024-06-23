7db0fc5e6a19307f121019dcc0968b46bfe97345
Trey Evans <lewis.r.evans@gmail.com>
2024-06-23 12:59:45 -0400

    Add certainty markers for source-based dependencies.

--- lib/sbom_on_rails/debian/oval.rb
+++ lib/sbom_on_rails/debian/oval.rb
@@ -2,6 +2,7 @@ require_relative "oval/parser"
 require_relative "oval/version_comparer"
 require_relative "oval/database"
 require_relative "oval/enricher"
+require_relative "oval/vulnerability_certainty_evaluator"
 
 module SbomOnRails
   module Debian
